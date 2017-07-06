f942c93166f1dfce392f3e6aa8bacad27396a5d5
marcionitao <marcionitao@gmail.com>
2017-07-06 07:03:55 +0200

    update and bug fix

--- src/main/java/pt/ulht/es/cookbook/controller/RecipeController.java
+++ src/main/java/pt/ulht/es/cookbook/controller/RecipeController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import pt.ulht.es.cookbook.model.Receita;
 import pt.ulht.es.cookbook.model.Tag;
 import pt.ulht.es.cookbook.service.ReceitaService;
